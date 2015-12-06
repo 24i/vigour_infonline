d2117ce704f49f6d91ed5238c2cd998dd4007ec3
ninjatux <valerio.barrila@gmail.com>
2015-12-06 01:20:03 +0100

    tests for mock, refactor, renaming

---
+++ test/browser/mockBridge.js
@@ -0,0 +1,29 @@
+'use strict'
+var bridge = require('vigour-wrapper/lib/bridge')
+
+bridge.label = 'mockBridge'
+bridge.mock = {
+  methods: {
+    init (opts, cb) {
+      console.log('init!')
+      setTimeout(function () {
+        cb && cb(null)
+      })
+    },
+    track (data, cb) {
+      // track the data!
+      console.log('track!')
+      setTimeout(function () {
+        cb && cb(null)
+      })
+    }
+  }
+}
+
+bridge.define({
+  send: function (pluginId, fnName, opts, cb) {
+    return bridge.mock.methods[fnName](opts, cb)
+  }
+})
+
+module.exports = bridge
