706d57e35761e0976bfc9b61f5c8eb575eaeb2de
Thomas Vervest <thomas@squarewolf.nl>
2015-12-10 02:33:01 +0100

    Integrated Infonline Android SDK

--- native/android/plugin-infonline/example/src/main/java/io/vigour/plugin/example/MainActivity.java
+++ native/android/plugin-infonline/example/src/main/java/io/vigour/plugin/example/MainActivity.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import com.fasterxml.jackson.jr.ob.impl.DeferredMap;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import de.infonline.lib.IOLEventType;
 import io.vigour.nativewrapper.plugin.core.BridgeEvents;
 import io.vigour.plugin.infonline.Infonline;
 
@@ -22,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        plugin = new Infonline();
+        plugin = new Infonline(this);
         plugin.setEventInterface(new BridgeEvents() {
 
             @Override public void receive(String event, String data, String pluginId) {
@@ -37,7 +40,14 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.log)
     public void log() {
-        // TODO: log data to Infonline
+        final String commentKey = "comment";
+        final String eventTypeKey = "eventType";
+        final String categoryKey = "category";
+        final DeferredMap dm = new DeferredMap(false);
+        dm.put("comment", "my comment");
+        dm.put("category", "custom category");
+        dm.put("eventType", IOLEventType.DataSucceeded.name());
+        plugin.track(dm);
     }
 
     private void feedback(String message) {
--- native/android/plugin-infonline/plugin-infonline/src/main/java/io/vigour/plugin/infonline/Infonline.java
+++ native/android/plugin-infonline/plugin-infonline/src/main/java/io/vigour/plugin/infonline/Infonline.java
@@ -1,33 +1,54 @@
 package io.vigour.plugin.infonline;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
 import com.fasterxml.jackson.jr.ob.JSON;
+import com.fasterxml.jackson.jr.ob.impl.DeferredMap;
 
 import java.io.IOException;
 
+import de.infonline.lib.IOLEventType;
+import de.infonline.lib.IOLSession;
+import io.vigour.nativewrapper.plugin.core.ActivityLifecycleListener;
 import io.vigour.nativewrapper.plugin.core.Plugin;
 
 /**
  * Created by Thomas on 3/12/15.
  */
-public class Infonline extends Plugin {
+public class Infonline extends Plugin implements ActivityLifecycleListener {
 	private static final String NAME = "infonline";
 	private static final String TAG = Infonline.class.getName();
 
-	public Infonline() {
+	public Infonline(@NonNull final Context context) {
 		super(NAME);
+		IOLSession.initIOLSession(context.getApplicationContext(), "OfferIdentifier", true);
 	}
 
 	public String init(final Boolean flag) {
 		return getString(flag);
 	}
 
-	public String track(final Object data) {
-		final String dataString = getString(data);
-		Log.d(TAG, "Tracking data: " + dataString);
-		return dataString;
+	public String track(final DeferredMap data) {
+		final String commentKey = "comment";
+		final String eventTypeKey = "eventType";
+		final String categoryKey = "category";
+
+		final String comment = data.containsKey(commentKey) ? (String)data.get(commentKey) : "";
+		final String eventTypeValue = data.containsKey(eventTypeKey) ? (String)data.get(eventTypeKey) : "";
+		final String category = data.containsKey(categoryKey) ? (String)data.get(categoryKey) : "";
+
+		final IOLEventType eventType;
+		try {
+			eventType = IOLEventType.valueOf(eventTypeValue);
+		} catch (IllegalArgumentException ex) {
+			sendError(ex.getLocalizedMessage());
+			return null;
+		}
+
+		IOLSession.logEvent(eventType, category, comment);
+		return null;
 	}
 
 	private String getString(@NonNull final Object status) {
@@ -38,5 +59,31 @@ public class Infonline extends Plugin {
 			sendError(e.getMessage());
 			return "";
 		}
+	}
+
+	public void terminateSession() {
+		IOLSession.terminateSession();
+	}
+
+	public void startSession() {
+		IOLSession.startSession();
+	}
+
+	@Override
+	public void onPause() {
+	}
+
+	@Override
+	public void onStop() {
+		IOLSession.onActivityStop();
+	}
+
+	@Override
+	public void onResume() {
+	}
+
+	@Override
+	public void onStart() {
+		IOLSession.onActivityStart();
 	}
 }
